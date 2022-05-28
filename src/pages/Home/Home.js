4cfb97f1213e8bfda51c144693da7bc9c5216e0d
Usama Muhammad <usamamuhammad45678@gmail.com>
2022-05-29 04:11:40 +0500

    progress home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import mobile_logo from '../../assets/images/mobile_logo.png'
+import arrowBlack from '../../assets/images/arrowBlack.png'
+import play_icon from '../../assets/images/play_icon.png'
+import mouse from '../../assets/images/mouse.png'
 
+const NavItem=({name, selected})=>{
+    return(
+        <div>
+            <div className={`${selected?'bg-[black] ':'bg-[#B1B1B1]'} mb-3 w-[40px] h-[20px] `} ></div>
+            <h5 className={`${selected?'font-bold text-[black] ':'font-normal text-[#B1B1B1]'} text-[14px] text-[] mr-5`} >{name}</h5>
+        </div>
+    )
+}
 
 function Home() {
   return (
@@ -26,9 +37,61 @@ function Home() {
         {/* top ends */}
 
         {/* award win start */}
-        <div className="awardCont">
-            <h1 className='uppercase font-bold text-center text-[85px] leading-[80px]' >award <br /> winning</h1>
+        <div className="homeMid">
+        <div className="awardCont mt-10">
+            <h1 className=' font-bold text-center text-[85px] leading-[80px]' >Award <br /> Winning</h1>
+            <h5 className='text-lg text-center mt-5 ' >Creative agency that provides  <span className='font-bold' >a full</span><br /> range of marketing services </h5>
         </div>
+        {/* buttons */}
+        <div className="homeMidButtons mt-5 flex justify-center">
+            {/* btn 1 */}
+        <div className="workWithUsBtn bg-[white] px-5 py-3 h-fit border-2 border-black rounded-[10px] w-fit flex items-center mr-5 ">
+            <h5 className='  text-sm font-bold text-center text-[11px] ' >
+
+            Work With Us
+            </h5>
+            <img src={arrowBlack} className=' object-contain ml-4' alt="" />
+        </div>
+        {/* btn1 ends */}
+
+        {/* btn 2 */}
+        <div className="playVideoBtn  bg-[#363636] px-5 py-3 h-fit border-2 rounded-[10px] w-fit flex items-center pr-9">
+        <img src={play_icon} className=' object-contain mr-4' alt="" />
+        <h5 className='  text-sm font-bold text-center text-[11px] text-[white] ' >
+        Play Video
+        </h5>
+       
+        </div>
+        {/* btn 2  ends */}
+        </div>
+        </div>
+        {/* award wins end */}
+
+        {/* mouse cont */}
+        <div className="mouseCont flex justify-center my-5">
+            <img src={mouse} className='object-contain' alt="" />
+        </div>
+        {/* mouse cont ends */}
+
+        {/* bottom start */}
+        <div className="homeBottom homeBottomBG w-[321px] h-[348px]  ">
+            {/* bottom nav */}
+            <div className="mobileNav fixed bottom-0 left-0 mt-[100%] bg-[#EFEFEF] w-[100vw] p-5 
+             ">
+                <div className='bg-[#B1B1B1] w-[90vw] h-[20px] ' >
+
+                </div>
+               <div className='flex mt-[-20px]' >
+               <NavItem name="Home" selected={true} />
+               <NavItem name="About" selected={false} />
+               <NavItem name="Home" selected={true} />
+
+               </div>
+            </div>
+            {/* bottom nav ends */}
+        </div>
+        {/* bottom ends */}
+
     </div>
   )
 }
