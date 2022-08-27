8ebacc9bb736cfc264c65eae81e0fe7f0210f708
usama-mk <usamamuhammad45678@gmail.com>
2022-08-27 09:12:28 +0500

    contact us p1 done

--- src/components/MobHeader.js
+++ src/components/MobHeader.js
@@ -4,16 +4,16 @@ import mobile_logo_white from '../assets/images/mobile_logo_white.png'
 import servicesHeader from '../assets/images/servicesHeader.png'
 
 
-function MobHeader({purpleTheme}) {
+function MobHeader({purpleTheme, redTheme}) {
   return (
-    <div style={{backgroundColor: purpleTheme?`#410D7F`:'', opacity:purpleTheme?1:'' }} className="topHeader pt-[20px] pb-[10px] pl-[6%] pr-[6%] flex justify-between w-[100%] bg-[#EFEFEF] opacity-80 h-fit">
+    <div style={{backgroundColor: purpleTheme?`#410D7F`:redTheme?'#FD4C3C':'', opacity:purpleTheme?1:'', opacity: redTheme?'100%':'' }} className="topHeader pt-[20px] pb-[10px] pl-[6%] pr-[6%] flex justify-between w-[100%] bg-[#EFEFEF] opacity-80 h-fit">
     <div className="logoCont flex">
         {
-          purpleTheme?
+          purpleTheme || redTheme?
           <img src={mobile_logo_white} className='object-contain w-[52px] h-[53px] '  alt="" />
           :
           <img src={mobile_logo} className='object-contain w-[52px] h-[53px] '  alt="" />}
-        <h3 style={{color:purpleTheme?"white":""}} className='uppercase font-bold text-left text-sm mt-[-5px] ml-[10px] leading-4 ' >pave <br />
+        <h3 style={{color:purpleTheme || redTheme?"white":""}} className='uppercase font-bold text-left text-sm mt-[-5px] ml-[10px] leading-4 ' >pave <br />
         creative <br />
         marketing <br />
         agency </h3>
--- src/pages/Mobile/Contact/ContactUs.js
+++ src/pages/Mobile/Contact/ContactUs.js
@@ -1,16 +1,70 @@
-import React from 'react'
-import MobHeader from '../../../components/MobHeader'
-import MobNavBar from '../../../components/MobNavBar'
-
+import React, { useState } from "react";
+import MobHeader from "../../../components/MobHeader";
+import MobNavBar from "../../../components/MobNavBar";
+import arrowBlack from "../../../assets/images/arrowBlack.png";
+import arrowYellow from "../../../assets/images/arrowYellow.png";
+import arrowWhite from "../../../assets/images/arrowWhite.png";
 function ContactUs() {
+  const [name, setName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [workEmail, setWorkEmail] = useState("");
   return (
-    <div style={{ height: "93vh", overflowY: "scroll" }}
-    className="flex flex-col  ">
-        <div className="  w-[100vw]  flex justify-center mb-12 ">
-        <MobHeader />
+    <div
+      style={{ height: "93vh", overflowY: "scroll" }}
+      className="flex flex-col  "
+    >
+      <div className="  w-[100vw]  flex justify-center  ">
+        <MobHeader redTheme={true} />
       </div>
-      <div className='mb-[100px] px-[6%]'>
-      ContactUs
+      <div className=" mt-[-10px] px-[6%] bg-[#FD4C3C] mb-[10px]  ">
+        <form className="mt-12 mb-5" >
+          <Input
+            label={`Name`}
+            type="text"
+            value={name}
+            onChange={setName}
+            placeholder={"Enter your full name here"}
+          />
+           <Input
+            label={`Company Name`}
+            type="text"
+            value={companyName}
+            onChange={setCompanyName}
+            placeholder={"PAVE Creative Agency"}
+          />
+          <Input
+            label={`Work Email`}
+            type="email"
+            value={workEmail}
+            onChange={setWorkEmail}
+            placeholder={"pave@company.com"}
+          />
+
+           <div className="mt-1.5 flex ">
+            
+            <FormButton
+              text={`Submit`}
+              bg={"#FEB52B"}
+              textColor={"black"}
+              p4={true}
+            />
+          </div>
+          
+        </form>
+      </div>
+      <div className="mt-[-10px] px-[6%] bg-[#410D7F] flex flex-col flex-1 pb-[100px]">
+        <h1 className=" text-[27px] font-bold leading-[31px] text-white mt-10 " >We’d love to hear from you.</h1>
+
+        <div className="text-left text-white px-10 mt-5" >
+        <h5 className="text-[14px] font-bold leading-[22px] " >CONTACT US</h5>
+        <h5 className="text-[20px] leading-[30px] opacity-70 mt-2 " >support@clubpave.com <br />
+(714) 332-0019</h5>
+
+<h5 className="text-[14px] font-bold leading-[22px] mt-10 " >By Appointment Only</h5>
+        <h5 className="text-[20px] leading-[30px] opacity-70 mt-2 " >1055 W 7th St, 33F, Los Angeles, CA 90017
+ <br />
+ 471 W. Lambert Road, Suite 103, Brea, CA 92821</h5>
+        </div>
       </div>
       <div
         className={`${
@@ -19,8 +73,58 @@ function ContactUs() {
       >
         <MobNavBar showNavBar={true} />
       </div>
-       </div>
-  )
+    </div>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
+
+const Input = ({ label, value, onChange, placeholder, type }) => {
+  return (
+    <div className="text-left mb-8 leading-[10px]  ">
+      <label className="text-[14px] text-white font-bold leading-[23px]  ">{label}</label>
+
+      <div className="leading-[23px] ">
+        <input
+          className="bg-transparent leading-[30px] border-b-[1px] border-[white] placeholder:text-[19px] placeholder:leading-[22px] placeholder:text-white placeholder:opacity-[35%]  w-[100%] focus:outline-none mt-2 "
+          type={type}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+};
+
+
+
+function FormButton({ textColor, bg, handleClick, p4, text, sendMessage }) {
+  return (
+    <div
+      style={{ borderColor: p4 ? bg : textColor, backgroundColor: bg }}
+      onClick={handleClick}
+      className={`workWithUsBtn bg-[${bg}] px-3 py-2 h-fit border-2 border-[${textColor}] rounded-[10px] w-fit flex items-center mr-5 `}
+    >
+      <h5
+        style={{ color: textColor }}
+        className={`text-[${textColor}] text-sm font-bold text-center text-[13px] font-sans `}
+      >
+        {text}
+      </h5>
+      {sendMessage ? (
+        <img
+          src={arrowWhite}
+          className=" object-contain ml-4 w-[12px] "
+          alt=""
+        />
+      ) : (
+        <img
+          src={textColor === "black" ? arrowBlack : arrowYellow}
+          className=" object-contain ml-4 w-[12px]"
+          alt=""
+        />
+      )}
+    </div>
+  );
+}
